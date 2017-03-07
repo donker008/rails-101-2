b277d2bfef120215a4884b6c887cc77cfe066a08
donker008 <richdonker@qq.com>
2017-03-07 17:23:37 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,6 +7,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
@@ -31,7 +32,7 @@ class GroupsController < ApplicationController
   def update
 
     if @group.update(group_params)
-      redirect_to groups_path,  notice: "update Success"
+      redirect_to group_path(@group),  notice: "update Success"
     else
       render :edit
     end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,34 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :destory]
 
   def index
     @posts = Post.all
   end
-  
+
+  def show
+    @group = Group.find(params[:group_id])
+    @posts = @group.posts
+  end
+
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.group = @group
+    @post.user = current_user
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      #flash[:alert] = "post failed!"
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
